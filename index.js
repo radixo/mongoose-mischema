6b9c902b495265221bb5f035815ae0b94e60964d
Walter Neto <wsouzant@gmail.com>
2017-01-30 05:39:13 -0200

    Finish to publish v1.0.0
    - Reestructured to MISchema object only
    - Created ofKind plugin
    - Created mischema.module function
    - Created MISchema.Schema getter

--- index.js
+++ index.js
@@ -6,35 +6,15 @@
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-/*
- * Module implementation
- *
- * use opts = { mo: {} } for mongoose Schema options
- */
-function MIStruct(tpName, obj, opts)
-{
-	// Force MIStruct instance
-	if (!(this instanceof MIStruct))
-		return new MIStruct(tpName, obj, opts);
-
-	// Generate attributes
-	this.obj = obj;
-	this.tpName = tpName;
-	this.opts = opts || {};
-
-	// Check opts
-	this.opts.deconflict = this.opts.deconflict || this.tpName + '_';
-}
-
-const schemaAdd = (task, schema, oKey, oVal, scope, tasks, schemas, dirties,
-    dirty) =>
+function schemaAdd(task, schema, oKey, oVal, scope, tasks, schemas, dirties,
+    dirty)
 {
 	var scItem;
 	var drItemNew;
-	var schValeuNew;
+	var schValueNew;
 	var tskNew;
 
-	if (oVal instanceof MIStruct) {
+	if (oVal instanceof MISchema) {
 		scItem = scope[oVal.tpName];
 		if (scItem !== undefined) {
 			schema[oKey] = scItem.objStruct;
@@ -45,16 +25,19 @@ const schemaAdd = (task, schema, oKey, oVal, scope, tasks, schemas, dirties,
 		tskNew = oVal;
 		drItemNew = [oKey, oVal.tpName];
 	} else if (oVal.constructor === Array && oVal.length == 1 &&
-	    oVal[0] instanceof MIStruct) {
+	    oVal[0] instanceof MISchema) {
 		scItem = scope[oVal[0].tpName];
 		if (scItem !== undefined) {
-			schema[oKey] = [scItem.mongooseSchema];
+			schema[oKey] = [scItem.getMongooseSchema()];
 			return false;
 		}
 
 		schValueNew = [];
 		tskNew = oVal[0];
 		drItemNew = [oKey, oVal[0].tpName];
+	} else {
+		schema[oKey] = oVal;
+		return false;
 	}
 
 	// Set values
@@ -75,7 +58,7 @@ const schemaAdd = (task, schema, oKey, oVal, scope, tasks, schemas, dirties,
 	return true; // It is dirty
 }
 
-const scopeAdd = (scope, objStruct) =>
+function scopeAdd(scope, objStruct)
 {
 	var scItem = scope[objStruct.tpName];
 
@@ -91,24 +74,28 @@ const scopeAdd = (scope, objStruct) =>
 	while (tasks.length > 0) {
 		var task = tasks[0];
 		var schema = schemas[0];
-		var obj = task.obj;
-		var objKeys = Object.keys(obj);
+		var obj, objKeys;
 		var dirty = false;
 
 		// Fix dirty schema
 		if (task === null) {
 			tasks.shift();
 			task = tasks[0];
+			obj = task.obj;
+			objKeys = Object.keys(obj);
 
 			for (var drItem of dirties[task.tpName]) {
 				if (schema[drItem[0]].constructor === Array)
 					schema[drItem[0]].push(scope[drItem[1]]
-					    .mongooseSchema);
+					    .getMongooseSchema());
 				else
 					schema[drItem[0]] = scope[drItem[1]]
 					    .objStruct;
 			}
 		} else {
+			obj = task.obj;
+			objKeys = Object.keys(obj);
+
 			// Looping through struct key, value
 			for (let oKey of objKeys) {
 				var oVal = obj[oKey];
@@ -123,8 +110,12 @@ const scopeAdd = (scope, objStruct) =>
 		if (!dirty) {
 			scope[task.tpName] = {
 				objStruct: schema,
-				mongooseSchema: new Schema(schema,
-				    task.opts.mo),
+				amso: task.opts.amso,
+				getMongooseSchema: () => {
+					return new Schema(
+					    scope[task.tpName].objStruct,
+					    scope[task.tpName].amso);
+				},
 			};
 
 			tasks.shift();
@@ -136,12 +127,15 @@ const scopeAdd = (scope, objStruct) =>
 	return rSchema;
 }
 
-const mergeSchemas = (sch0, sch1, usedKeys, deconflict) =>
+function mergeSchemas(sch0, sch1, usedKeys, deconflict)
 {
 	var keys = Object.keys(sch1);
 
 	// Looping through new schema (sch1)
 	for (let key of keys) {
+		if (key === '_kinds')
+			continue;
+
 		let val = sch1[key];
 
 		// Checks for key been used conflict
@@ -160,31 +154,137 @@ const mergeSchemas = (sch0, sch1, usedKeys, deconflict) =>
 	}
 }
 
-const MISchema = (objs, opts) =>
+function mergeImplementeds(imps0, imps1)
 {
-	// Check if objs is an Array
-	if (objs.constructor !== Array)
-		return undefined;
+	var keys1 = Object.keys(imps1);
 
+	for (let key of keys1)
+		imps0[key] = true;
+}
+
+function genMISchema(objs, implementeds)
+{
 	var schema = {} // The resulting schema
 	var scope = {}; // stores each compiled schema
 	var usedKeys = {}; // for resolving conflicted keys
 
-	// Generate schemas
-	for (var i = 0; i < structs.length; i++)
-		schemas.push({});
-
-	// Looping through structs Array
-	for (let objStruct of structs) {
-		// ignore non MIStruct objects
-		if (!(objStruct instanceof MIStruct))
+	// Looping through objs Array
+	for (let objStruct of objs) {
+		// ignore non MISchema objects
+		if (!(objStruct instanceof MISchema))
 			continue;
 
 		mergeSchemas(schema, scopeAdd(scope, objStruct), 
-		    objStruct.opts.deconflict);
+		    usedKeys, objStruct.opts.deconflict);
+		mergeImplementeds(implementeds, objStruct.implementeds);
 	}
 
-	return new Schema(schema, opts);
+	return schema;
+}
+
+function genKinds(implementeds)
+{
+	var keys = Object.keys(implementeds);
+	var rArr = []
+
+	for (let key of keys)
+		rArr.push(key);
+
+	return { type: Array, 'default': rArr };
+}
+
+/*
+ * Module implementation
+ *
+ * opts:
+ * 	deconflict: String, for deconflict key names
+ * 	mso: {}, for mongoose Schema options
+ * 	amso: {}, for auto-generated mongoose options
+ */
+function MISchema()
+{
+	// Declare arguments
+	var argc = 0;
+	var tpName, inherits, obj, opts;
+
+	// Arguments assert
+	tpName = arguments[argc++];
+	if (arguments[argc].constructor === Array)
+		inherits = arguments[argc++];
+	obj = arguments[argc++];
+	opts = arguments[argc++];
+
+	// Force MISchema instance
+	if (!(this instanceof MISchema)) {
+		if (inherits)
+			return new MISchema(tpName, inherits, obj, opts);
+		else
+			return new MISchema(tpName, obj, opts);
+	}
+
+	// Generate attributes
+	this.tpName = tpName;
+	this.implementeds = {};
+	if (inherits)
+		this.obj = Object.assign(genMISchema(inherits,
+		    this.implementeds), obj);
+	else
+		this.obj = obj;
+	this.implementeds[this.tpName] = true;
+	this.obj._kinds = genKinds(this.implementeds);
+	this.opts = opts || {};
+	this.mongooseSchema = undefined;
+
+	// Check opts
+	this.opts.deconflict = this.opts.deconflict || this.tpName + '_';
+	this.opts.amso = this.opts.amso || { _id: false };
+
+	// Getters
+	Object.defineProperty(this, 'Schema', {
+		get: function() {
+			if (!this.mongooseSchema)
+				this.mongooseSchema = new Schema(this.obj,
+				    this.opts.mso);
+
+			return this.mongooseSchema;
+		},
+	});
+}
+
+/*
+ * Register plugin
+ */
+mongoose.plugin(function MISchemaPlugin(schema, options)
+{
+	var ofKind = function(arr, kind) {
+		var kindName;
+
+		if (kind instanceof MISchema)
+			kindName = kind.tpName;
+		else
+			kindName = kind;
+
+		return (arr.indexOf(kindName) != -1);
+	}
+
+	// Instance method
+	schema.methods.ofKind = function(kind) {
+		return ofKind(this._kinds, kind);
+	};
+
+	// Static method
+	schema.statics.ofKind = function(kind) {
+		return ofKind(this.schema.path('_kinds').options['default'],
+		    kind);
+	}
+});
+
+function model(name, schema, collection, skipInit)
+{
+	if (schema instanceof MISchema)
+		schema = schema.Schema;
+
+	return mongoose.model(name, schema, collection, skipInit);
 }
 
 /*
@@ -192,8 +292,8 @@ const MISchema = (objs, opts) =>
  */
 function MongooseMISchema()
 {
-	this.MIStruct = MIStruct;
 	this.MISchema = MISchema;
+	this.model = model;
 }
 
 module.exports = exports = new MongooseMISchema();
